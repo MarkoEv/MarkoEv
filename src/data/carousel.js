02ad56bb238b0e61d11c908b5105dd8662360df5
MarkoEv <markodevp@gmail.com>
2026-03-29 00:17:25 -0600

    importacion de imagenes => cambio de ruta

--- src/data/carousel.js
+++ src/data/carousel.js
@@ -1,15 +1,15 @@
-import javascript from './images/lenguajes/javascript.png';
-import react from './images/lenguajes/react.png';
-import nodejs from './images/lenguajes/nodejs.png';
-import php from './images/lenguajes/php-logo.png';
-import mysql from './images/lenguajes/mysql-logo.png';
-import postgresql from './images/lenguajes/postgresql.png';
-import css3 from './images/lenguajes/css3.png';
-import html from './images/lenguajes/html.png';
-import tailwind from './images/lenguajes/tailwind_css.png';
-import bootstrap from './images/lenguajes/bootstrap.png';
-import github from './images/lenguajes/github.png';
-import laravel from './images/lenguajes/laravel-white.png';
+import javascript from '../assets/images/lenguajes/javascript.png';
+import react from '../assets/images/lenguajes/react.png';
+import nodejs from '../assets/images/lenguajes/nodejs.png';
+import php from '../assets/images/lenguajes/php-logo.png';
+import mysql from '../assets/images/lenguajes/mysql-logo.png';
+import postgresql from '../assets/images/lenguajes/postgresql.png';
+import css3 from '../assets/images/lenguajes/css3.png';
+import html from '../assets/images/lenguajes/html.png';
+import tailwind from '../assets/images/lenguajes/tailwind_css.png';
+import bootstrap from '../assets/images/lenguajes/bootstrap.png';
+import github from '../assets/images/lenguajes/github.png';
+import laravel from '../assets/images/lenguajes/laravel-white.png';
 
 export const lenguajes = [
   { icono: javascript },
