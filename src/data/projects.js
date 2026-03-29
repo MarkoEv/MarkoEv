1fc76280d4ebaedf641008673956e3254a30ad43
MarkoEv <markodevp@gmail.com>
2026-03-29 15:31:14 -0600

    add Obejto projects => carrusel

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,70 +1,76 @@
-import curp from "../assets/images/projects/curp.png";
-import rickmorty from "../assets/images/projects/rickmorty.png";
-import tasksmanager from "../assets/images/projects/tasksmanager.png";
-import snapventas from "../assets/images/projects/snapventas.png";
-import slavdigital from "../assets/images/projects/slavdigital.png";
-import dfa from "../assets/images/projects/dfa.png";
+import curp from '../assets/images/projects/curp.jpg';
+import rickmorty from '../assets/images/projects/rickmorty.jpg';
+import tasksmanager from '../assets/images/projects/tasksmanager.jpg';
+import snapventas from '../assets/images/projects/snapventas.svg';
+import slavdigital from '../assets/images/projects/slavdigital.jpg';
+import dfa from '../assets/images/projects/fda.jpg';
 
 export const proyectos = [
   {
     id: 1,
-    nombre: "CURP Generator",
+    nombre: 'SnapVentas',
     descripcion:
-      "Generador de CURP mexicano con validación en tiempo real y efecto de tipeo.",
-    stack: ["Vite", "Tailwind", "Swiper.js"],
-    imagen: curp,
-    github: "https://github.com/MarkoEv/generador-de-curp",
-    live: "https://generador-de-curp.vercel.app",
+      'Sistema de punto de venta (POS) con gestión de productos, ventas y reportes.',
+    stack: ['PHP', 'MySQL', 'AJAX'],
+    imagen: snapventas,
+    github: '',
+    category: 'Comercial',
+    live: 'https://snapventas.com',
   },
   {
     id: 2,
-    nombre: "Rick & Morty Explorer",
+    nombre: 'Slav Digital',
     descripcion:
-      "Explorador de personajes y episodios con fetch, paginación y animaciones.",
-    stack: ["Vite", "JavaScript", "Tailwind", "DaisyUI"],
-    imagen: rickmorty,
-    github: "https://github.com/MarkoEv/rickmorty-explorer",
-    live: "https://rickmorty-explorer-iota.vercel.app",
+      'Rastreador de vehiculos, rutas y administrar los vehiculos en tiempo real',
+    stack: ['Laravel', 'MySQL', 'PHP'],
+    imagen: slavdigital,
+    github: '',
+    category: 'Escolar',
+    live: 'https://slav.digital',
   },
   {
     id: 3,
-    nombre: "Tasks Manager",
+    nombre: 'Rick & Morty Explorer',
     descripcion:
-      "Gestor de tareas con modo oscuro, drag & drop y persistencia en localStorage.",
-    stack: ["React", "Vite", "Tailwind"],
-    imagen: tasksmanager,
-    github: "https://github.com/MarkoEv/tasksmanager",
-    live: "https://tasksmanager.vercel.app",
+      'Explorador de personajes y episodios, para la comunidad de fans de la serie.',
+    stack: ['Vite', 'JavaScript', 'Tailwind'],
+    imagen: rickmorty,
+    github: 'https://github.com/MarkoEv/rickmorty-explorer',
+    category: 'Comunidad',
+    live: 'https://rickmorty-explorer-iota.vercel.app',
   },
   {
     id: 4,
-    nombre: "SnapVentas",
-    descripcion:
-      "Sistema de punto de venta (POS) con gestión de productos, ventas y reportes.",
-    stack: ["PHP", "MySQL"],
-    imagen: snapventas,
-    github: "https://github.com/MarkoEv/snapventas",
-    live: "https://snapventas.com",
+    nombre: 'CURP Generator',
+    descripcion: 'Generador de CURP mexicano con validación en tiempo real.',
+    stack: ['Vite', 'Tailwind', 'Swiper.js'],
+    imagen: curp,
+    github: 'https://github.com/MarkoEv/generador-de-curp',
+    category: 'Hobby',
+    live: 'https://generador-de-curp.vercel.app',
   },
   {
     id: 5,
-    nombre: "Slav Digital",
+    nombre: 'Tasks Manager',
     descripcion:
-      "Rastreador de vehiculos con funciones para asignar rutas y administrar los vehiculos en tiempo real",
-    stack: ["Laravel", "MySQL", "PHP"],
-    imagen: slavdigital,
-    github: "https://github.com/MarkoEv/slav-digital",
-    live: "https://slav.digital",
+      'Un Gestor de tareas gratuito con persistencia en localStorage.',
+    stack: ['React', 'Vite', 'Tailwind'],
+    imagen: tasksmanager,
+    github: 'https://github.com/MarkoEv/tasksmanager',
+    category: 'Hobby',
+    live: 'https://tasksmanager.vercel.app',
   },
+
   {
     id: 6,
-    nombre: "Simulador de AFD",
+    nombre: 'Simulador de AFD',
     descripcion:
-      "Simulador de Autómatas Finitos Deterministas con visualización de grafos.",
-    stack: ["Vue.js", "Bootstrap", "Cytoscape.js"],
+      'Simulador de Autómatas Finitos Deterministas con visualización de grafos.',
+    stack: ['Vue.js', 'Bootstrap', 'Cytoscape.js'],
     imagen: dfa,
     github:
-      "https://github.com/MarkoEv/Simulador-de-Automatas-Finitos-Deterministas-AFD",
-    live: "",
+      'https://github.com/MarkoEv/Simulador-de-Automatas-Finitos-Deterministas-AFD',
+    category: 'Hobby',
+    live: '',
   },
 ];
