b7a813ff9f781ffa77db7e6c28ae0007ae45b124
MarkoEv <markodevp@gmail.com>
2026-03-29 15:30:24 -0600

    update => mezclación de objetos

---
+++ src/data/techs.js
@@ -0,0 +1,112 @@
+export const techs = [
+  {
+    nombre: 'HTML',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
+  },
+  {
+    nombre: 'CSS',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg',
+  },
+  {
+    nombre: 'JavaScript',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+  },
+  {
+    nombre: 'React',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+  },
+  {
+    nombre: 'Vue.js',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg',
+  },
+  {
+    nombre: 'Bootstrap',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg',
+  },
+  {
+    nombre: 'Tailwind',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg',
+  },
+  {
+    nombre: 'Vite',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vite/vite-original.svg',
+  },
+  {
+    nombre: 'Swiper.js',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+  },
+  {
+    nombre: 'GSAP',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+  },
+  {
+    nombre: 'Node.js',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg',
+  },
+  {
+    nombre: 'PHP',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg',
+  },
+  {
+    nombre: 'Laravel',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/laravel/laravel-original.svg',
+  },
+  {
+    nombre: 'MySQL',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg',
+  },
+  {
+    nombre: 'PostgreSQL',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg',
+  },
+  {
+    nombre: 'Git',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg',
+  },
+
+  {
+    nombre: 'Google',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/google/google-original.svg',
+  },
+  {
+    nombre: 'Netlify',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/netlify/netlify-original.svg',
+  },
+  {
+    nombre: 'Linux',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg',
+  },
+  {
+    nombre: 'Hostinger',
+    icono: 'https://cdn.simpleicons.org/hostinger/673DE6',
+  },
+
+  {
+    nombre: 'Shopify',
+    icono: 'https://cdn.simpleicons.org/shopify/7AB55C',
+  },
+  {
+    nombre: 'VS Code',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg',
+  },
+];
