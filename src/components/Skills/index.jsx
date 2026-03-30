ef1c2fd5f1529c463886cba856bb3f412fbd7995
MarkoEv <markodevp@gmail.com>
2026-03-30 00:33:46 -0600

    Add componente Skills => Renderiza la sección de habilidades con la gridKills

---
+++ src/components/Skills/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { GridSkills } from './GridSkills';
+
+function Skills() {
+  return (
+    <section aria-labelledby="Skills-title" className="bg-black py-16">
+      <div className="text-center">
+        <header className="space-y-4 text-center">
+          <h2 className="text-4xl font-bold text-center justify-center flex gap-2">
+            Mis
+            <span className="text-4xl font-bold text-green-400">
+              Habilidades
+            </span>
+          </h2>
+          <p className="text-lg font-semibold text-white">Skills Fullstack</p>
+        </header>
+        <div className="w-full">
+          <div className="flex justify-center">
+            <GridSkills />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export { Skills };
