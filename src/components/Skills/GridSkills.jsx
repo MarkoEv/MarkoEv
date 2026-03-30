090ffcd272944a84bd355b4f38116b48c8cb7233
MarkoEv <markodevp@gmail.com>
2026-03-30 01:27:29 -0600

    Update responsive desing

--- src/components/Skills/GridSkills.jsx
+++ src/components/Skills/GridSkills.jsx
@@ -5,37 +5,39 @@ function GridSkills() {
   return (
     <div
       className="
-      grid 
-      grid-cols-2 
-      sm:grid-cols-2 
-      md:grid-cols-3 
-      lg:grid-cols-4 
-      xl:grid-cols-5 
-      gap-4 sm:gap-5
-      w-full
-      px-4 sm:px-12 lg:px-36      
-    "
+        grid 
+        grid-cols-3 
+        sm:grid-cols-4 
+        md:grid-cols-3 
+        lg:grid-cols-4 
+        xl:grid-cols-5 
+        gap-4 sm:gap-5
+        w-full
+        px-4 sm:px-12 lg:px-36
+      "
     >
       {skills.map((i) => (
         <article
           key={i.id}
           className="
-            flex items-center gap-3 
+            flex flex-col items-center text-center
+            sm:flex-row sm:text-left
+            gap-3 
             rounded-2xl 
-            p-4 
+            p-2 sm:p-3 lg:p-4 
             bg-white/5 
             border border-white/10
-            jus
+            hover:bg-white/10 
+            transition-all duration-300
           "
         >
-          {/* ICONO */}
           <div
             className="
-            flex h-10 w-10 sm:h-12 sm:w-12 
-            items-center justify-center 
-            rounded-xl 
-            bg-white/10
-          "
+              flex h-10 w-10 sm:h-12 sm:w-12 
+              items-center justify-center 
+              rounded-xl 
+              bg-white/10
+            "
           >
             <img
               src={i.icono}
@@ -44,13 +46,12 @@ function GridSkills() {
             />
           </div>
 
-          {/* TEXTO */}
-          <div className="text-left">
-            <h2 className="text-sm sm:text-base font-semibold text-white leading-tight">
+          <div>
+            <h2 className="hidden sm:block text-sm sm:text-base font-semibold text-white leading-tight">
               {i.nombre}
             </h2>
 
-            <p className="text-xs sm:text-sm text-gray-400 line-clamp-2">
+            <p className="hidden md:block text-xs sm:text-sm text-gray-400 line-clamp-2">
               {i.descripcion}
             </p>
           </div>
