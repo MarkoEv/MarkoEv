1c10f2e09cb3a51b2fa26e63c11a6b078574dcb5
MarkoEv <markodevp@gmail.com>
2026-03-30 00:33:58 -0600

    Update objeto skills

---
+++ src/data/skills.js
@@ -0,0 +1,109 @@
+export const skills = [
+  {
+    id: 1,
+    nombre: 'Node.js',
+    descripcion: 'JS en servidor',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg',
+  },
+  {
+    id: 2,
+    nombre: 'Laravel',
+    descripcion: 'Framework PHP',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/laravel/laravel-original.svg',
+  },
+  {
+    id: 3,
+    nombre: 'PHP',
+    descripcion: 'Backend web',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg',
+  },
+  {
+    id: 4,
+    nombre: 'MySQL',
+    descripcion: 'Base de datos',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg',
+  },
+  {
+    id: 5,
+    nombre: 'PostgreSQL',
+    descripcion: 'BD avanzada',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg',
+  },
+
+  {
+    id: 6,
+    nombre: 'React',
+    descripcion: 'UI dinámica',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+  },
+  {
+    id: 7,
+    nombre: 'JavaScript',
+    descripcion: 'Lenguaje web',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+  },
+  {
+    id: 8,
+    nombre: 'Vue.js',
+    descripcion: 'Framework UI',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg',
+  },
+  {
+    id: 9,
+    nombre: 'Tailwind',
+    descripcion: 'CSS utilidades',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg',
+  },
+  {
+    id: 10,
+    nombre: 'Bootstrap',
+    descripcion: 'CSS componentes',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg',
+  },
+  {
+    id: 11,
+    nombre: 'CSS',
+    descripcion: 'Estilos web',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg',
+  },
+  {
+    id: 12,
+    nombre: 'HTML',
+    descripcion: 'Estructura web',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
+  },
+
+  {
+    id: 13,
+    nombre: 'Git',
+    descripcion: 'Versionado',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg',
+  },
+  {
+    id: 15,
+    nombre: 'Linux',
+    descripcion: 'Sistema base',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg',
+  },
+  {
+    id: 16,
+    nombre: 'Vite',
+    descripcion: 'Build tool',
+    icono:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vite/vite-original.svg',
+  },
+];
