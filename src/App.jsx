4e819c2da70efc70abdd3c7a14b7edc0ba1d26f1
MarkoEv <markodevp@gmail.com>
2026-03-30 00:29:28 -0600

    Add componente Skills

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Blogs } from './components/Blogs';
 import { Footer } from './components/Footer';
 import { Hero } from './components/Hero';
 import { Projects } from './components/Projects';
+import { Skills } from './components/Skills';
 
 function App() {
   return (
     <>
       <Hero />
       <Projects />
+      <Skills />
     </>
   );
 }
