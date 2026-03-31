efd289f3197581c68a2dcf6ce717728f38ac58cf
MarkoEv <markodevp@gmail.com>
2026-03-30 19:37:48 -0600

    update fuente principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { Blogs } from './components/Blogs';
+import { Blogs } from './components/Blog';
 import { Footer } from './components/Footer';
+import { Header } from './components/Header';
 import { Hero } from './components/Hero';
 import { Projects } from './components/Projects';
 import { Skills } from './components/Skills';
@@ -7,9 +8,11 @@ import { Skills } from './components/Skills';
 function App() {
   return (
     <>
+      <Header />
       <Hero />
       <Projects />
       <Skills />
+      <Blogs />
     </>
   );
 }
